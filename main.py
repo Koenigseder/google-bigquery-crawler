fe7bd5adc93f3d6eba0235639033412880aa7eb7
Koenigseder <kevin.koenigseder@outlook.com>
2023-02-24 10:09:44 +0100

    Add banner and catch exceptions on startup

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import json
+import warnings
 
 from google.cloud import bigquery, bigquery_datatransfer
 from google.protobuf.json_format import MessageToDict
 
+warnings.filterwarnings("ignore", "Your application has authenticated using end user credentials")
+
 
 def main():
-
-    bigquery_client = bigquery.Client()
+    try:
+        bigquery_client = bigquery.Client()
+    except BaseException as e:
+        print(e)
+        return
 
     project_ids = []
 
@@ -224,5 +230,46 @@ def search_scheduled_queries_with_table_name(project_id, search_string: str):
     # ------------- End of scanning scheduled-queries destination tables -------------
 
 
+def print_banner():
+    g = [[' ', '╭', '─', '╮'], [' ', '│', ' ', '╮'], [' ', '╰', '─', '╯']]
+    b = [[' ', '┌', '─', '╮'], [' ', '├', '─', '┤'], [' ', '└', '─', '╯']]
+    q = [[' ', '╭', '─', '╮'], [' ', '│', ' ', '│'], [' ', '╰', '─', '╲']]
+    space = [['   '], ['   '], ['   ']]
+    c = [[' ╭', '─', '┐'], [' │  '], [' ╰', '─', '┘']]
+    r = [[' ', '┌', '─', '╮'], [' ', '├', '─', '╯'], [' ', '┴', ' ', '┴']]
+    a = [[' ', '┌', '─', '┐'], [' ', '├', '─', '┤'], [' ', '┴', ' ', '┴']]
+    w = [[' ', '┬', ' ', '┬'], [' ', '│', '│', '│'], [' ', '└', '┴', '┘']]
+    l = [[' ', '┬', ' ', ' '], [' ', '│', ' ', ' '], [' ', '┴', '─', '┘']]
+    e = [[' ', '┌', '─', '┐'], [' ', '├', '┤', ' '], [' ', '└', '─', '┘']]
+
+    banner = [g, b, q, space, c, r, a, w, l, e, r]
+    final = []
+    print('\r')
+    init_color = 36
+    txt_color = init_color
+    cl = 0
+
+    for charset in range(0, 3):
+        for pos in range(0, len(banner)):
+            for i in range(0, len(banner[pos][charset])):
+                clr = f'\033[38;5;{txt_color}m'
+                char = f'{clr}{banner[pos][charset][i]}'
+                final.append(char)
+                cl += 1
+                txt_color = txt_color + 36 if cl <= 3 else txt_color
+
+            cl = 0
+
+            txt_color = init_color
+        init_color += 31
+
+        if charset < 2: final.append('\n   ')
+
+    print(f"   {''.join(final)}")
+    print("\033[0m")
+
+
+
 if __name__ == "__main__":
+    print_banner()
     main()
